a6a2add5ca9f3c4186c4226aa453b0df2271770d
AungPhyoHein89 <aungsihein2014@gmail.com>
2026-05-28 21:46:45 +0630

    Add custom method charm

--- 03_lecture.py
+++ 03_lecture.py
@@ -1,3 +1,4 @@
+"""
 class Student:
   def __init__(self, name, house):
     if not name:
@@ -24,5 +25,52 @@ def get_student():
       print(f"Error : {e}. Try again!\n")
       continue
     
+if __name__ == "__main__":
+  main()
+"""
+
+class Student:
+  def __init__(self, name, house, patronus):
+    if not name:
+      raise ValueError("Missing Name.")
+    if house not in ["Gryffindor","Hufflepuff", "Ravenclaw", "Slytherin"]:
+      raise ValueError("Invalid House")
+    self.name = name
+    self.house = house
+    self.patronus = patronus
+  
+  def __str__(self):
+    return f"{self.name} from {self.house}"
+  
+  def charm(self):
+    match self.patronus:
+      case "Stag":
+        return "🦌"
+      case "Otter":
+        return "🦦"
+      case "Jack Russell terrier":
+        return "🐶"
+      case "Phoenix":
+        return "🦅"
+      case _:
+        return "🪄"
+      
+def main():
+  student = get_student()
+  print("\n---- Student Info ----")
+  print(student)
+  print(f"Expecto Patronum! {student.charm()}")
+
+def get_student():
+  while True:
+    try:
+      name = input(f"Name: ")
+      house = input(f"House: ")
+      patronus = input(f"Patronus (e.g., Stag, Otter): ")
+      return Student(name, house, patronus)
+    except ValueError as e:
+      print(f"Error {e}.Try Again\n")
+      continue
+
 if __name__ == "__main__":
   main()
