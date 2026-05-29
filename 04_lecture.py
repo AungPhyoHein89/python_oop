813350e931d0c31d353c05c90ecfc817a1e2500f
AungPhyoHein89 <aungsihein2014@gmail.com>
2026-05-29 23:28:48 +0630

    Encapsulation (Data validation of name and house) and while true to ask user again and again

--- 04_lecture.py
+++ 04_lecture.py
@@ -25,7 +25,7 @@ def get_student():
 if __name__ == "__main__":
   main()
 """
-
+"""
 class Student:
   def __init__(self, name, house):
     if not name:
@@ -62,6 +62,53 @@ def main():
 def get_student():
   name = input(f"Name: ")
   house = input(f"House: ")
+  return Student(name, house)
+
+if __name__ == "__main__":
+  main()
+"""
+
+class Student:
+  def __init__(self, name, house):
+    self.name = name
+    self.house = house
+
+  @property
+  def name(self):
+    return self._name
+  
+  @name.setter
+  def name(self, name):
+    if not name:
+      raise ValueError("Missing Name")
+    self._name = name
+
+  @property
+  def house(self):
+    return self._house
+  
+  @house.setter
+  def house(self, house):
+    if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+      raise ValueError("Invaild House.")
+    self._house = house
+
+  def __str__(self):
+    return f"{self.name} from {self.house}"
+
+def main():
+  while True:
+    try:  
+      student = get_student()
+      #student.house = "Number Four ..."
+      print(student)
+      break
+    except ValueError as e:
+      print(f"Error: {e}.Try Again\n")
+
+def get_student():
+  name = input("Name: ")
+  house = input("House: ")
   return Student(name, house)
 
 if __name__ == "__main__":
