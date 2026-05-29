3fca1c72f65643969d2264d5c77f53e6af147127
AungPhyoHein89 <aungsihein2014@gmail.com>
2026-05-29 09:29:37 +0630

    insecure Encapsulation in OOP

---
+++ 04_lecture.py
@@ -0,0 +1,24 @@
+class Student:
+  def __init__(self, name, house):
+    if not name:
+      raise ValueError("Missing Name.")
+    if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+      raise ValueError("Invalide House.")
+    self.name = name
+    self.house = house
+
+  def __str__(self):
+    return f"{self.name} from {self.house}"
+
+def main():
+  student = get_student()
+  student.house = "Number Four ..."
+  print(student)
+
+def get_student():
+  name = input(f"Name: ")
+  house = input(f"House: ")
+  return Student(name, house)
+
+if __name__ == "__main__":
+  main()
